26d5a919fa87b9f298b45ea454e6578022103270
Augustin <aroche@nordex-online.com>
2014-03-14 10:29:37 +0100

    creation of the DB

--- qpackageproject.py
+++ qpackageproject.py
@@ -20,7 +20,52 @@
  ***************************************************************************/
 """
 
+from PyQt4.QtCore import *
+from PyQt4.QtXml import *
+from pyspatialite import dbapi2 as db
+import os.path
+
 
 class QPackageProject:
     def __init__(self, projectFile):
         self.projectFile = projectFile
+        
+        self.loadProject()
+        self.createDB()
+        
+        
+    def loadProject(self):
+        f = QFile(self.projectFile)
+        if not f.open(QIODevice.ReadOnly | QIODevice.Text):
+            msg = "Cannot read file %s:\n%s." % (self.projectFile, f.errorString())
+            #self.processError.emit(msg)
+            return
+
+        self.doc = QDomDocument()
+        setOk, errorString, errorLine, errorColumn = doc.setContent(f, True)
+        if not setOk:
+            msg = "Parse error at line %d, column %d:\n%s" % (errorLine, errorColumn, errorString)
+            # self.processError.emit(msg)
+            return False
+
+        f.close()
+        return True
+        
+    def createDB(self):
+        (root, ext) = os.path.splitext(self.projectFile)
+        self.dbConnection = db.connect(root + '.sqlite')
+        cur = self.dbConnection.cursor()
+        cur.execute("SELECT initspatialmetadata()")
+        self.dbConnection.commit()
+        
+        
+    def findLayerInProject(self, layerElement, layerName):
+        child = layerElement.firstChildElement()
+        while not child.isNull():
+            nm = child.firstChildElement("layername")
+            if nm.text() == layerName:
+                return child
+            child = child.nextSiblingElement()
+        return None
+        
+    
